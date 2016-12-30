bc4d92b39c36d3373f298017682fa507c18138ab
Ravi Budhu <ravibudhu@gmail.com>
2016-12-29 20:08:39 -0500

    Adds STATIC_ROOT to settings

--- tenso/settings.py
+++ tenso/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 FONT_PATH = os.path.join(BASE_DIR, 'impact.ttf')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
