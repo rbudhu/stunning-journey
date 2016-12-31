e3b31c8a4c019888f9564c49ea4ffbde74ee7129
mena <mena@josiahisgay.com>
2016-12-30 23:41:08 -0700

    Better fix for cropperjs image reloading

--- web/static/web/js/tenso.js
+++ web/static/web/js/tenso.js
@@ -12,24 +12,27 @@ $("#id_image").change(function(event) {
 	    if (!$( '#img-upload-warning' ).hasClass('hidden')) {
 		$( '#img-upload-warning' ).addClass('hidden');
 	    }
-	    
+
 	    var reader = new FileReader();
 	    reader.onload = function(){
 		var output = document.getElementById('img-preview');
 		output.src = reader.result;
+
 		if (output.cropper) {
 		    output.cropper.replace(output.src);
 		}
-		var cropper = new Cropper(output, {
-		    background: false,
-		    crop: function(e) {
-			var box = [e.detail.x, e.detail.y, 
-				   e.detail.x + e.detail.width,
-				   e.detail.y + e.detail.height];
-			var box_json = JSON.stringify(box);
-			$("#id_box").val(box_json);
-		    }
-		});
+		else {
+		    var cropper = new Cropper(output, {
+			background: false,
+			crop: function(e) {
+			    var box = [e.detail.x, e.detail.y,
+				       e.detail.x + e.detail.width,
+				       e.detail.y + e.detail.height];
+			    var box_json = JSON.stringify(box);
+			    $("#id_box").val(box_json);
+			}
+		    });
+		}
             };
 	    reader.readAsDataURL(event.target.files[0]);
 	}
